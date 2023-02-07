97cfd5faab6b167c9d6456ac2da527ccee2314e9
dheeraj <dheerajck18@gmail.com>
2023-02-08 03:48:46 +0530

    Some users cant be mentioned outside a chat where they are present, so message send to user to asking to verify everything is forwarded to Admin channel so admin can directly connect with them from mention, adjusted keyboard message as next message with callback data having group id and message id so it can find the original data and send it as needed to Broker channel when needed, this is a better way for a real usecases as users with no username might use the bot

--- bot_folder/plugins/tgbot/callback_handlers.py
+++ bot_folder/plugins/tgbot/callback_handlers.py
@@ -8,25 +8,35 @@ from bot_folder.helpers import add_keyboad_button_and_send_text_message
 async def user_submit(client, callback_query):
     await callback_query.answer(cache_time=100)
     await callback_query.message.edit_text(callback_query.message.text, reply_markup=None)
+
     response = "Thank you for your request, we have begun sourcing the best offers across the verified brokers network, and will get back to you as soon as possible."
     await client.send_message(callback_query.message.chat.id, response)
 
     # send it to admins
     async for group_id in AdminChannel.objects.all():
+        forwarded_message = await callback_query.message.forward(group_id)
         await add_keyboad_button_and_send_text_message(
-            client, group_id, callback_query.message.text, "SEND TO BROKER CHANNELS", "SEND"
+            client,
+            group_id,
+            "Do you want to send above text to broker channels ??",
+            "SEND TO BROKER CHANNELS",
+            f"SEND {group_id} {forwarded_message.id}",
         )
 
 
-@Client.on_callback_query(filters.regex("SEND"))
+@Client.on_callback_query(filters.regex(r"SEND -?\d+(\d+)? -?\d+(\d+)?"))
 async def admin_choice(client, callback_query):
     await callback_query.answer(cache_time=100)
     await callback_query.message.edit_text(callback_query.message.text, reply_markup=None)
     response = "Quote sent to broker channel (link of message in broker channel)"
     await client.send_message(callback_query.message.chat.id, response)
 
-    new_data = callback_query.message.text.split("\n")[4:]
+    data = callback_query.data
+    _, group_id, message_id = data.split()
+
+    new_message = await client.get_messages(group_id, message_id)
+    new_data = new_message.text.split("\n")[4:]
     new_data = "\n".join(new_data).strip()
 
     async for group_id in BrokerChannel.objects.all():
-        await client.send_message(group_id, group_id)
+        await client.send_message(group_id, new_data)
