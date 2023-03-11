de5184f49f2b7170afc0a46992646aa12506354a
dheeraj <dheerajck18@gmail.com>
2023-03-11 17:02:47 +0530

    Refactored broker user handler by using get_user_details.py

--- bot_folder/plugins/tgbot/broker_users.py
+++ bot_folder/plugins/tgbot/broker_users.py
@@ -6,71 +6,54 @@ from pyrogram import enums
 from shared_config import shared_object
 
 
-async def get_user_details(client, message):
-    try:
-        user = message.command[1] if len(message.command) == 2 else ""
-        user_object = await client.get_users(user)
-    except Exception as e:
-        # If bot cant find user from the information provided <username or user ID>,
-        # send a clear error message why bot cant find the user
-        await message.reply(e)
-        return None
-    else:
-        return user_object
+from bot_folder.helpers import get_user_details
 
 
 @Client.on_message(shared_object.clients["bot_admins"] & filters.command("adduser", prefixes="!"))
 async def add_broker_user_handler(client, message):
-    if len(message.command) != 2:
-        await message.reply("Specify user")
-        message.stop_propagation()
+    """
+    This is a message handler to add new bot admin.
+    The user must be super admin in order use this command.
+    The command syntax is: !add_bot_admin <username or user ID>
+    """
 
-    try:
-        user = message.command[1]
-        user_object = await client.get_chat(user)
-    except Exception as e:
-        await message.reply(e)
-        message.stop_propagation()
+    user_object = await get_user_details(client, message)
 
-    if user_object.type == enums.ChatType.PRIVATE:
+    if user_object:
+        # If the <username or user ID> is valid, add that user as a bot admin
+        name = user_object.first_name + (user_object.last_name or "")
         try:
-            user_id = user_object.id
-            full_name = user_object.first_name + (user_object.last_name or "")
-            await BrokerChannel.objects.acreate(group_id=user_id, title=full_name, is_user=True)
-            await message.reply(f"Added user {full_name} to broker groups")
+            await BrokerChannel.objects.acreate(group_id=user_object.id, title=name, is_user=True)
         except IntegrityError:
-            await message.reply(f"Added user {full_name} to broker groups")
+            await message.reply(f"{name} is already a bot user")
+        else:
+            shared_object.clients["bot_admins"].add(user_object.id)
+            await message.reply(f"Added user {name} to broker groups")
 
-    else:
-        await message.reply("Specify user not group / channel")
     message.stop_propagation()
 
 
 @Client.on_message(shared_object.clients["bot_admins"] & filters.command("removeuser", prefixes="!"))
 async def remove_broker_user_handler(client, message):
-    user = message.command[1]
-    try:
-        user_object = await client.get_chat(user)
-    except Exception as e:
-        await message.reply(e)
-        message.stop_propagation()
+    user_object = await get_user_details(client, message)
 
-    if user_object.type == enums.ChatType.PRIVATE:
-        full_name = user_object.first_name + (user_object.last_name or "")
-        await BrokerChannel.objects.filter(group_id=user_object.id).adelete()
-        await message.reply(f"Removed {full_name} from broker groups if he was a broker user")
-    else:
-        await message.reply("Specify user not group / channel")
+    if user_object:
+        name = user_object.first_name + (user_object.last_name or "")
+        await BrokerChannel.objects.filter(group_id=user_object.id, is_user=True).adelete()
+        shared_object.clients["bot_admins"].discard(user_object.id)
+        await message.reply(f"Removed {name} from broker groups if he was a broker user")
+
     message.stop_propagation()
 
 
 @Client.on_message(shared_object.clients["bot_admins"] & filters.command("listuser", prefixes="!"))
 async def list_broker_user_handler(client, message):
-    output = ""
+    output = "List of broker users\n"
+
     async for channel in BrokerChannel.objects.filter(is_user=True):
         output += f"{channel.title } `{channel.group_id}`\n"
-    if output != "":
-        await message.reply(output, quote=False)
-    else:
-        await message.reply("No Broker users are present in db", quote=False)
+
+    output = output if output != "List of broker users\n" else "No broker users in the db"
+    await message.reply(output, quote=False)
+
     message.stop_propagation()
